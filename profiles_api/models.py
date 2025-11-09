e8a8e0e8b31730d3018c83ae02311c0cef227061
ayush <jainayus1139@gmail.com>
2025-11-09 19:22:52 +0530

    Configured and migrate Profil model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,110 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, Group, Permission
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+  """Manager or user profile"""
+
+  def create_user(self, email, name, password=None):
+    """Create a new user profile"""
+
+    if not email:
+      raise ValueError ('User must have email address')
+    
+    email = self.normalize_email(email)
+    user = self.model(email=email, name=name)
+    
+    # this will encrypt the password
+    user.set_password(password)
+
+    user.save(using=self._db)
+
+    return user
+  
+
+  def create_superuser(self, email, name, password):
+    """Create and save super user details"""
+    user = self.create_user(email, name, password)
+
+    user.is_superuser = True
+    user.is_staff = True
+    user.save(using=self._db)
+
+    return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+  """Database model for user in the system"""
+
+  email = models.EmailField(max_length=255, unique=True)
+  name = models.CharField(max_length=255)
+  is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
+
+
+  # Add the unique related_name and related_query_name arguments
+  groups = models.ManyToManyField(
+      Group,
+      blank=True,
+      help_text=(
+          'The groups this user belongs to. A user will get all permissions '
+          'granted to each of their groups.'
+      ),
+      related_name="profiles_manager_grp", # Unique related name
+      related_query_name="profiles_manager_grp",
+  )
+  user_permissions = models.ManyToManyField(
+      Permission,
+      blank=True,
+      help_text='Specific permissions for this user.',
+      related_name="profiles_manager_perm", # Unique related name
+      related_query_name="profiles_manager_perm",
+  )
+
+
+
+  # USER MODEL MANAGER CLASS
+  objects = UserProfileManager()
+
+  # using email as username_field
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['name']
+
+  def get_full_name(self):
+    """Return user full name"""
+    return self.name
+  
+  def get_short_name(self):
+    "Return Short name of User"
+    return self.name
+
+  def __str__(self):
+    """Return string representation of USER"""
+    return self.email
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
