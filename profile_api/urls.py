52ab25f81b9397a56dd927c0c3ad6512748caed5
ayush <jainayus1139@gmail.com>
2025-11-14 11:55:46 +0530

    Tested rest_api view

---
+++ profile_api/urls.py
@@ -0,0 +1,26 @@
+
+
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
+    
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+  """Testing rest api"""
+
+  def get(self, request, format=None):
+    """Return a list of ApiViews Features"""
+
+    an_apiview = [
+      'Uses http funtions as methods',
+      'Is similar to traditionl Django View',
+      'Give you most control over you app',
+      'is mapped manually URLs',
+    ]
+
+    # every https func must have a response b/c it is reuired to return a response in django framework
+    # Every response must be a List or Dict b/c it's converted into JSON
+    return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
